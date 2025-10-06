b58c9886d36560018efff945ecf96dd62f760735
vikashisexploringai <vikashisexploringai@gmail.com>
2025-10-06 19:35:04 +0530

    Create timer.js

---
+++ js/core/timer.js
@@ -0,0 +1,42 @@
+// Timer management
+export class Timer {
+    constructor() {
+        this.startTime = null;
+        this.elapsedTime = 0;
+        this.interval = null;
+        this.onTick = null;
+    }
+
+    start(onTick = null) {
+        this.startTime = Date.now() - this.elapsedTime;
+        this.onTick = onTick;
+        
+        this.interval = setInterval(() => {
+            this.elapsedTime = Date.now() - this.startTime;
+            if (this.onTick) {
+                this.onTick(this.getFormattedTime());
+            }
+        }, 1000);
+    }
+
+    stop() {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+    }
+
+    reset() {
+        this.stop();
+        this.elapsedTime = 0;
+        this.startTime = null;
+    }
+
+    getFormattedTime() {
+        return Utils.formatTime(Math.floor(this.elapsedTime / 1000));
+    }
+
+    getElapsedSeconds() {
+        return Math.floor(this.elapsedTime / 1000);
+    }
+}
